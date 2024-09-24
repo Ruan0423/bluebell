7289fcd5cfd31f9425f62a8aae2abf0032e9f309
Ruan0423 <0802traveler@gmail.com>
2024-09-24 19:09:04 +0800

    优化

--- dao/mysql/mysql.go
+++ dao/mysql/mysql.go
@@ -2,31 +2,33 @@ package mysql
 
 import (
 	"fmt"
-	"web_framework/settings"
-
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
-
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+	"web_framework/settings"
 )
-var Db *gorm.DB
+
+var db *sqlx.DB
 
 func Init(cfg *settings.Mysqlconfig) (err error) {
-
-
-	dsn :=fmt.Sprintf("%s:%s@(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-	cfg.Username,
-	cfg.Password,
-	cfg.Host,
-	cfg.Port,
-	cfg.Database,
-)
-	fmt.Println(dsn)
-	Db,err = gorm.Open("mysql",dsn)
-	if err!=nil{
-		fmt.Println(err)
-		return err
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		cfg.Username,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.Database,
+		)
+	// 也可以使用MustConnect连接不成功就panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		zap.L().Error("connect DB failed ", zap.Error(err))
+		return
 	}
-	
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	return
+}
 
-	return nil
-} 
+func Close()  {
+	_ = db.Close()
+}
